e48fdeadb3d35b3dbea90924301226828f73052e
oleg golovin <oleggolo@googlemail.com>
2024-12-17 00:04:41 +0100

    nnbnb

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -11,7 +11,6 @@ module.exports = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  
   const lock = m.contract("Lottery", [unlockTime], {
     value: lockedAmount,
   });
